846c4cf49f212d1e9a81cdf199d84af1ffab46b8
Kayhlacr <kayhla@student.dev-island.org>
2024-03-07 19:48:01 -1000

    implement navigation with links to all routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,40 @@
 // import { useState } from "react";
 import "./App.css";
-import { Routes } from "react-router-dom";
+import { NavLink, Routes, useNavigate } from "react-router-dom";
 import { Route } from "react-router-dom";
 import Red from "./routes/Red";
 import Blue from "./routes/Blue";
 import Green from "./routes/Green";
 
 function App() {
-  // const [count, setCount] = useState(0)
+  const navigate = useNavigate();
+  const pageRoutes = ["/", "/green", "/blue"];
 
   return (
     <>
+      <nav className="navigation">
+        <ul className="nav-menu">
+          <li>
+            <NavLink to="/">Home</NavLink>
+          </li>
+          <li>
+            <NavLink to="/green">Green Page</NavLink>
+          </li>
+          <li>
+            <NavLink to="/blue">Blue Page</NavLink>
+          </li>
+        </ul>
+        <button
+          onClick={() => {
+            const randomPageIndex = Math.floor(
+              Math.random() * pageRoutes.length
+            );
+            navigate(pageRoutes[randomPageIndex]);
+          }}
+        >
+          Random Page
+        </button>
+      </nav>
       <Routes>
         <Route path="/" element={<Red />} />
         <Route path="/green" element={<Green />} />
